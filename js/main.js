df01d130b54bd9022705b8a0b298ad57312fbf58
Nicole <nicolecar14@gmail.com>
2018-05-03 13:55:44 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
+$(document).ready(function(){
+    Chart.defaults.global.legend.display = false;
+    Chart.defaults.global.tooltips.enabled = false;
+    new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+            labels: ["money1", "money2"],
+            datasets: [
+                {
+                    label: "avg orders this week",
+                    backgroundColor: ["#889aaa", "#687787"],
+                    data: [10,30]
+                }
+            ]
+        },
+        options: {
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }   
+            }
+        }
+    });
+
+    new Chart(document.getElementById("doughnut-chart-2"), {
+        type: 'doughnut',
+        data: {
+            labels: ["money1", "money2"],
+            datasets: [
+                {
+                    label: "avg orders this week",
+                    backgroundColor: ["#889aaa", "#687787"],
+                    data: [30,10]
+                }
+            ]
+        },
+        options: {
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }   
+            }
+        }
+    });
+});
+
+
 $( "#hamburger" ).click(function() {
-    console.log("clicked...");
     $( "#left-nav" ).toggle();
     $( "#hamburger-icon" ).toggleClass( "mdi-menu mdi-close" );
   });
-
-  var donutEl = document.getElementById("donut").getContext("2d");
-  var donut = new Chart(donutEl, {
-      type: 'doughnut',
-      data : [
-        {
-            value: 300,
-            color:"#F7464A",
-            highlight: "#FF5A5E",
-            label: "Red"
-        },
-        {
-            value: 50,
-            color: "#46BFBD",
-            highlight: "#5AD3D1",
-            label: "Green"
-        },
-        {
-            value: 100,
-            color: "#FDB45C",
-            highlight: "#FFC870",
-            label: "Yellow"
-        }
-    ],
-    // Options
-    options: {
-        segmentShowStroke : true,
-        segmentStrokeColor : "#fff",
-        segmentStrokeWidth : 2,
-        percentageInnerCutout : 50,
-        animationSteps : 100,
-        animationEasing : "easeOutBounce",
-        animateRotate : true,
-        animateScale : false,
-        responsive: true,
-        maintainAspectRatio: true,
-        showScale: true,
-        animateScale: true
-    }
-});
